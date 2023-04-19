05c6de655cf48e13a6de96219f7966f128db7408
parkhyeonji <iamphj3@naver.com>
2023-04-19 16:32:02 +0900

    feat : 카테고리 필터링 기능 구현

--- week2/AppleStore/product.js
+++ week2/AppleStore/product.js
@@ -6,16 +6,16 @@ const $ = (selector) => document.querySelector(selector);
 // product 섹션
 const productSection = $('.product');
 
-// function loadProduct() {
-// product 추가
-productList.forEach(({ productName, hashtagList, url, alt }) => {
-  // hashtagList 목록 li 요소로 변환
-  const hashtagItems = hashtagList
-    .map((hashtag) => `<li class="hashtag_item">${hashtag}</li>`)
-    .join('');
+function loadProduct() {
+  // product 추가
+  productList.forEach(({ category, productName, hashtagList, url, alt }) => {
+    // hashtagList 목록 li 요소로 변환
+    const hashtagItems = hashtagList
+      .map((hashtag) => `<li class="hashtag_item">${hashtag}</li>`)
+      .join('');
 
-  const productCard = `
-      <article class="product_card">
+    const productCard = `
+      <article class="product_card" data-category="${category}">
         <header>
           <h2>${productName}</h2>
         </header>
@@ -32,10 +32,29 @@ productList.forEach(({ productName, hashtagList, url, alt }) => {
       </article>
     `;
 
-  productSection.insertAdjacentHTML('beforeend', productCard);
-});
-// }
+    productSection.insertAdjacentHTML('beforeend', productCard);
+  });
+}
 
-// window.onload = () => {
-//   loadProduct();
-// };
+const categories = $$('.category_item');
+
+categories.forEach((categoryItem) => {
+  categoryItem.addEventListener('click', () => {
+    const categoryId = categoryItem.dataset.category;
+    filterProducts(categoryId);
+  });
+});
+
+function filterProducts(categoryId) {
+  $$('.product_card').forEach((productCard) => {
+    if (categoryId === 'All' || productCard.dataset.category === categoryId) {
+      productCard.style.display = 'flex';
+    } else {
+      productCard.style.display = 'none';
+    }
+  });
+}
+
+window.onload = () => {
+  loadProduct();
+};
