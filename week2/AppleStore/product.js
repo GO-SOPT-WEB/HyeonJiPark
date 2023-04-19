118c96039ec9bade159aabda35950f4cb620e09b
parkhyeonji <iamphj3@naver.com>
2023-04-19 14:51:32 +0900

    chore : 불필요한 클래스명 제거

--- week2/AppleStore/product.js
+++ week2/AppleStore/product.js
@@ -6,38 +6,36 @@ const $ = (selector) => document.querySelector(selector);
 // product 섹션
 const productSection = $('.product');
 
-function loadProduct() {
-  // product 추가
-  productList.forEach(({ productName, hashtagList, url, alt }) => {
-    // hashtagList 목록 li 요소로 변환
-    const hashtagItems = hashtagList
-      .map((hashtag) => `<li class="hashtag_item">${hashtag}</li>`)
-      .join('');
+// function loadProduct() {
+// product 추가
+productList.forEach(({ productName, hashtagList, url, alt }) => {
+  // hashtagList 목록 li 요소로 변환
+  const hashtagItems = hashtagList
+    .map((hashtag) => `<li class="hashtag_item">${hashtag}</li>`)
+    .join('');
 
-    const productCard = `
+  const productCard = `
       <article class="product_card">
         <header>
-          <h2 class="product_name">${productName}</h2>
+          <h2>${productName}</h2>
         </header>
         <section class="hashtag_wrapper">
-          <ul class="hashtag_list">
+          <ul>
             ${hashtagItems}
           </ul>
           <button type="button">+</button>
         </section>
-        <section class="product_image">
-          <img src="${url}" alt="${alt}" />
-        </section>
+        <img src="${url}" alt="${alt}" />
         <footer>
           <img class="ic_bookmark" src="icons/ic_bookmark.svg" alt="북마크" />
         </footer>
       </article>
     `;
 
-    productSection.insertAdjacentHTML('beforeend', productCard);
-  });
-}
+  productSection.insertAdjacentHTML('beforeend', productCard);
+});
+// }
 
-window.onload = () => {
-  loadProduct();
-};
+// window.onload = () => {
+//   loadProduct();
+// };
