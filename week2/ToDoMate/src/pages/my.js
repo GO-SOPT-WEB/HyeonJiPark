32efb56dc8532d6c2b397770f492fe94cd7c1bfc
parkhyeonji <iamphj3@naver.com>
2023-04-21 13:06:46 +0900

    feat : 드래그 앤 드롭 순서 변경 기능 구현

--- week2/ToDoMate/src/pages/my.js
+++ week2/ToDoMate/src/pages/my.js
@@ -1,4 +1,5 @@
-import { IcMy } from '../assets/icons';
+import { $, $$ } from '../utils/querySelector';
+
 function My($container) {
   this.$container = $container;
 
@@ -12,16 +13,58 @@ function My($container) {
             <section class="myCategory">
               <h1>MY CATEGORY</h1>
               <ul>
-                <li class="today" draggable="true">TODAY</li>
-                <li class="todo" draggable="true">TODO</li>
-                <li class="study" draggable="true">STUDY</li>
-                <li class="sopt" draggable="true">SOPT</li>
+                <li class="category today" draggable="true">TODAY</li>
+                <li class="category todo" draggable="true">TODO</li>
+                <li class="category study" draggable="true">STUDY</li>
+                <li class="category sopt" draggable="true">SOPT</li>
               </ul>
             </section>
           </main>
         `;
-  };
 
+    const categories = document.querySelectorAll('.category');
+
+    let draggedItem = null;
+
+    categories.forEach((category) => {
+      category.addEventListener('dragstart', () => {
+        draggedItem = category;
+        category.classList.add('dragging');
+      });
+
+      category.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const bounding = category.getBoundingClientRect();
+        const offset = bounding.y + bounding.height / 2;
+        if (e.clientY > offset) {
+          category.classList.add('drop-after');
+        } else {
+          category.classList.remove('drop-after');
+        }
+      });
+
+      category.addEventListener('dragleave', () => {
+        category.classList.remove('drop-after');
+      });
+
+      category.addEventListener('drop', () => {
+        const dropAfter = category.classList.contains('drop-after');
+        if (draggedItem !== category) {
+          if (dropAfter) {
+            category.after(draggedItem);
+          } else {
+            category.before(draggedItem);
+          }
+        }
+        category.classList.remove('drop-after');
+      });
+
+      category.addEventListener('dragend', () => {
+        draggedItem.classList.remove('dragging');
+        draggedItem = null;
+      });
+    });
+  };
   this.render();
 }
 
