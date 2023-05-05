e7d677bf7fb13eaae905c3c18ff360a1ec679634
parkhyeonji <iamphj3@naver.com>
2023-05-05 20:53:34 +0900

    chore : css 속성 개행 추가

--- week3/Zzangu/src/pages/Game.tsx
+++ week3/Zzangu/src/pages/Game.tsx
@@ -190,15 +190,19 @@ const StMode = styled.nav`
     transition: transform 150ms cubic-bezier(0, 0, 0.58, 1);
 
     &::before {
-      position: absolute;
       content: '';
-      width: 100%;
-      height: 100%;
+
+      position: absolute;
       top: 0;
       left: 0;
+
+      width: 100%;
+      height: 100%;
+
       background: ${({ theme }) => theme.colors.Zzangu_DarkPink};
       border-radius: inherit;
       box-shadow: 0 0 0 0.2rem ${({ theme }) => theme.colors.Zzangu_PinkBorder};
+
       transform: translate3d(0, 0.75rem, -1rem);
       transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
         box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
@@ -232,6 +236,7 @@ const StMode = styled.nav`
     right: 11%;
     overflow: hidden;
     z-index: 10;
+
     padding: 1rem 4rem;
 
     color: ${({ theme }) => theme.colors.Zzangu_YellowBorder};
@@ -239,6 +244,7 @@ const StMode = styled.nav`
     ${({ theme }) => theme.fonts.ZZangu_Button1};
     border: 0.3rem solid ${({ theme }) => theme.colors.Zzangu_YellowBorder};
     border-radius: 5rem;
+
     transition: all 0.3s ease-in-out;
 
     &:hover {
@@ -247,40 +253,48 @@ const StMode = styled.nav`
     }
     &::before {
       content: '';
+      display: block;
+      position: absolute;
+      top: 0;
+      left: -10%;
+      z-index: -12;
+      opacity: 1;
+
       width: 0%;
       height: 100%;
-      display: block;
+
       background: ${({ theme }) => theme.colors.Zzangu_YellowBorder};
-      position: absolute;
+
       transform: skewX(-20deg);
-      left: -10%;
-      opacity: 1;
-      top: 0;
-      z-index: -12;
       transition: all 0.7s cubic-bezier(0.77, 0, 0.175, 1);
       box-shadow: 0.2rem 0rem 1.4rem rgba(0, 0, 0, 0.6);
     }
     &::after {
       content: '';
+      display: block;
+      position: absolute;
+      top: 0;
+      left: -10%;
+      z-index: -15;
+      opacity: 0;
+
       width: 0%;
       height: 100%;
-      display: block;
+
       background: ${({ theme }) => theme.colors.Zzangu_White};
-      position: absolute;
+
       transform: skewX(-20deg);
-      left: -10%;
-      opacity: 0;
-      top: 0;
-      z-index: -15;
       transition: all 0.4s cubic-bezier(0.2, 0.95, 0.57, 0.99);
       box-shadow: 0.2rem, 0rem 1.4rem rgba(0, 0, 0, 0.6);
     }
     &:hover::before {
       opacity: 1;
+
       width: 116%;
     }
     &:hover::after {
       opacity: 1;
+
       width: 120%;
     }
   }
