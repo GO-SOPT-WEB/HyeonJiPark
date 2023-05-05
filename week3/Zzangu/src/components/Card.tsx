2d40461dc0c1bf4941c8de02177f15dc418ae168
parkhyeonji <iamphj3@naver.com>
2023-05-05 16:07:06 +0900

    feat : 카드 뒤집을 때 flip 애니메이션

--- week3/Zzangu/src/components/Card.tsx
+++ week3/Zzangu/src/components/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { ImgZzanguBack } from '../assets/images';
 
@@ -13,15 +13,19 @@ interface CardProps {
 
 const Card = ({ id, src, alt, isFlipped, handleClick }: CardProps) => {
   return (
-    <StCardWrapper onClick={() => handleClick(id)}>
-      {isFlipped ? <img src={src} alt={alt} /> : <img src={ImgZzanguBack} alt='카드뒷면' />}
+    <StCardWrapper onClick={() => handleClick(id)} isFlipped={isFlipped}>
+      {isFlipped ? (
+        <img className='cardFront' src={src} alt={alt} />
+      ) : (
+        <img className='cardBack' src={ImgZzanguBack} alt='카드뒷면' />
+      )}
     </StCardWrapper>
   );
 };
 
 export default Card;
 
-const StCardWrapper = styled.article`
+const StCardWrapper = styled.article<{ isFlipped: boolean }>`
   display: flex;
 
   width: 20rem;
@@ -31,9 +35,24 @@ const StCardWrapper = styled.article`
 
   background-color: ${({ theme }) => theme.colors.Zzangu_Yellow};
   border-radius: 2rem;
+  transition: transform 0.6s;
 
   & > img {
     width: 100%;
     border-radius: 2rem;
   }
+
+  & > .cardFront {
+    transform: rotateY(0deg);
+  }
+
+  & > .cardBack {
+    transform: rotateY(180deg);
+  }
+
+  ${({ isFlipped }) =>
+    isFlipped &&
+    css`
+      transform: rotateY(180deg);
+    `}
 `;
