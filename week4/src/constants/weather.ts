9edd28cd70fd8530689866f69ddfcf52252e8de9
parkhyeonji <iamphj3@naver.com>
2023-05-11 20:39:46 +0900

    chore : WEATHER_TYPE 추가

---
+++ week4/src/constants/weather.ts
@@ -0,0 +1,56 @@
+export interface weather {
+  description: string;
+  imgURL: string;
+}
+
+export const WEATHER_TYPE: weather[] = [
+  {
+    description: 'clear sky',
+    imgURL: 'https://i.pinimg.com/236x/fa/80/5a/fa805af9a1642835c51cdff085ab40f4.jpg',
+  },
+  {
+    description: 'few clouds',
+    imgURL: 'https://i.pinimg.com/236x/c3/86/ef/c386efcc2b99aa8caafb1dacf376afc3.jpg',
+  },
+  {
+    description: 'scattered clouds',
+    imgURL: 'https://i.pinimg.com/236x/b5/3d/42/b53d4202c8e25e4e6bc3ebb81385c2ee.jpg',
+  },
+  {
+    description: 'overcast clouds',
+    imgURL: 'https://i.pinimg.com/236x/b5/3d/42/b53d4202c8e25e4e6bc3ebb81385c2ee.jpg',
+  },
+
+  {
+    description: 'heavy intensity rain',
+    imgURL: 'https://i.pinimg.com/564x/91/b7/3c/91b73cecbdfb2ab5177523e66ae0cef7.jpg',
+  },
+  {
+    description: 'broken clouds',
+    imgURL: 'https://i.pinimg.com/236x/12/7b/27/127b27107c1e54db5bc5fa5f1e2b4b47.jpg',
+  },
+  {
+    description: 'shower rain',
+    imgURL: 'https://i.pinimg.com/236x/ab/3c/47/ab3c47b72e03dff11b40bd640a0f9d7c.jpg',
+  },
+  {
+    description: 'rain',
+    imgURL: 'https://i.pinimg.com/236x/32/86/d9/3286d9a785a9db5e15f7422a96d757fc.jpg',
+  },
+  {
+    description: 'thunderstorm',
+    imgURL: 'https://i.pinimg.com/236x/5c/ed/0f/5ced0fae87292feba7fe373881e3937d.jpg',
+  },
+  {
+    description: 'snow',
+    imgURL: 'https://i.pinimg.com/236x/27/c0/5b/27c05bdab26655679a13fff25ad42b14.jpg',
+  },
+  {
+    description: 'mist',
+    imgURL: 'https://i.pinimg.com/564x/4a/87/7f/4a877fdd886e23abf98eda3438ac09ed.jpg',
+  },
+  {
+    description: 'moderate rain',
+    imgURL: 'https://i.pinimg.com/236x/32/86/d9/3286d9a785a9db5e15f7422a96d757fc.jpg',
+  },
+];
