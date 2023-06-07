62a11c435ad1ccca1ee43db95527c341d14f13ac
parkhyeonji <iamphj3@naver.com>
2023-06-07 11:34:51 +0900

    chore : mode, score Atom 생성

--- week3/Zzangu/src/recoil/atom.ts
+++ week3/Zzangu/src/recoil/atom.ts
@@ -1,1 +1,13 @@
 import { atom } from 'recoil';
+
+import { EASY_MODE } from '../datas/mode';
+
+export const ModeState = atom<number>({
+  key: 'Mode',
+  default: EASY_MODE,
+});
+
+export const ScoreState = atom<number>({
+  key: 'Score',
+  default: 0,
+});
