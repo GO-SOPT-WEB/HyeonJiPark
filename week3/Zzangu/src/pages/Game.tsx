79b52610ae659cd7822e58b066803ab9ac1976b9
parkhyeonji <iamphj3@naver.com>
2023-06-07 12:36:08 +0900

    chore : 사용하지 않는 모듈 선언 제거

--- week3/Zzangu/src/pages/Game.tsx
+++ week3/Zzangu/src/pages/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 
