bf74ac5f50de86c90b2dd9d024d56530030e41ef
parkhyeonji <iamphj3@naver.com>
2023-06-07 14:01:21 +0900

    chore : vercel.json 추가

--- week3/Zzangu/vite.config.ts
+++ week3/Zzangu/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // If you have a tsconfig.json file, you can add its path here
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
